40583864220b92aed05b57fbb20549d326c53599
AmanYadavFeb22 <ay415282@gmail.com>
2025-09-22 13:08:08 +0530

    some basic question on array

--- array.js
+++ array.js
@@ -1,4 +1,4 @@
-//Q1) find duplicate elements from an unsorted array
+// Q1) find duplicate elements from an unsorted array
 let arr = [1, 2, 3, 2, 5, 7, 8, 2, 5, 4, 2, 6, 5, 7, 8, 4, 2];
 function findduplicate(arr) {
   let res = [];
@@ -15,4 +15,20 @@ function findduplicate(arr) {
 }
 let duplicate = findduplicate(arr);
 console.log(duplicate);
+
+
+//Q2) Find the length of unique element from an sorted array
+let arr1 = [1, 1, 2, 2, 2, 3, 3]
+function finddup(arr1) {
+  let j = 0
+  for (let i = 1; i < arr1.length; i++) {
+    if (arr1[i] > arr1[j] && arr1[i] != arr1[j]) {
+      arr1[j + 1] = arr1[i]
+      j++
+    }
+  }
+  return j+1
+}
+let val = finddup(arr1)
+console.log(val)
 
