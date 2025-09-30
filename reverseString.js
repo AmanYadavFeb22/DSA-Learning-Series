b6d50a83690b24ec3d68905dacca5b0460119cd8
AmanYadavFeb22 <ay415282@gmail.com>
2025-09-30 16:19:24 +0530

    Solving reverse String problems

---
+++ reverseString.js
@@ -0,0 +1,14 @@
+let arr =['h','e','l','l','o']
+
+function revstr(arr) {
+    for (let i =0; i<arr.length/2; i++) {
+        let temp = arr[i]
+        arr[i] = arr[arr.length - 1 - i]
+        arr[arr.length-1-i]=temp
+
+
+    }
+    return arr
+}
+let val = revstr(arr)
+console.log(val)
